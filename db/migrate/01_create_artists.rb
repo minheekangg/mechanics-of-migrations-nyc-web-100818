0e9fc065fd25ab0f7061f4415e377f1243f219e3
Min Hee  Park <minheekangg@gmail.com>
2018-10-19 19:54:05 +0000

    automatically backed up by learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -8,3 +8,21 @@ class CreateArtists < ActiveRecord::Migration
     end
   end
 end
+
+
+connection = ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
+
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS artists (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  genre TEXT,
+  age INTEGER,
+  hometown TEXT
+  )
+SQL
+
+ActiveRecord::Base.connection.execute(sql)
