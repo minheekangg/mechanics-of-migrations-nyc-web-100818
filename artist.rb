51e1743b2dc3db4317ece6a25e5f46c57e80181a
Min Hee  Park <minheekangg@gmail.com>
2018-10-19 19:47:06 +0000

    automatically backed up by learn

--- artist.rb
+++ artist.rb
@@ -1,2 +1,12 @@
 class Artist < ActiveRecord::Base
+
+  a = Artist.new(name: 'Jon')
+# => #<Artist id: nil, name: "Jon", genre: nil, age: nil, hometown: nil>
+ 
+a.age = 30
+# => 30
+ 
+a.save
+# => true
+
 end
